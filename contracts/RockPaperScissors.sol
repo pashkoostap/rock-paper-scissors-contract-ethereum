39e2cbb4d20e4e51c3af2dbabdf54eb43b2c32c0
pashkoostap <pashko.ostap@gmail.com>
2018-07-11 23:09:53 +0300

    updated contract

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -1,20 +1,26 @@
 pragma solidity ^0.4.17;
 
+
+
 contract RockPaperScissors {
-  address[] private players;
+  struct Player {
+    address adr;
+    string bit;
+  }
+
   address public manager;
+
+  mapping (address => Player) public players;
+
   function RockPaperScissors() public {
     manager = msg.sender;
   }
 
-  function play() public payable {
+  function play(string bit) public payable {
     require(msg.value >= 0.1 ether);
     require(msg.value <= 1 ether);
-    players.push(msg.sender);
-  }
 
-  function getPlayers() public view returns (address[]) {
-    return players;
+    players[msg.sender] = Player({ adr: msg.sender, bit: bit });
   }
 
   function getManager() public view returns (address) {
